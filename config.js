76db503b9c4da5ce155b13012bf34d32e7ea098f
Anenth <anenthvishnu@gmail.com>
2016-12-30 11:38:45 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    REFERENCE_IMAGES_PATH: '../jarvis/vr_actual_1482906732',
+    TEST_IMAGES_PATH: '../jarvis/test_1482980355',
+    DIFF_IMAGES_PATH: './diff_images',
+    MIS_MATCH_THRESHOLD: 1,
+    RESEMABLE_SETTINGS: {
+        errorColor: {
+            red: 155,
+            green: 100,
+            blue: 155
+        },
+        errorType: 'movement',
+        transparency: 0.3
+    }
+}
---
+++ vr_test.js
@@ -0,0 +1,116 @@
+const resemble = require('node-resemble-js');
+const fs = require('fs');
+const fse = require('fs-extra');
+const streamToPromise = require('stream-to-promise');
+const parseArgs = require('minimist');
+
+const config = require('./config');
+let {
+    REFERENCE_IMAGES_PATH,
+    TEST_IMAGES_PATH} = config;
+
+const {
+    DIFF_IMAGES_PATH,
+    MIS_MATCH_THRESHOLD,
+    RESEMABLE_SETTINGS
+} = config;
+
+function getConfigFromArgs() {
+    var argsOptions = parseArgs(process.argv.slice(2), {
+        boolean: ['h', 'help'],
+        string: ['config']
+    });
+
+    if (argsOptions.h || argsOptions.help) {
+        console.log("Use refs - Reference images path \n Use test - Test images path"); //eslint-disable-line
+        process.exit();
+    }
+
+    let {refs, test} = argsOptions['_'][0];
+    REFERENCE_IMAGES_PATH = refs || REFERENCE_IMAGES_PATH;
+    TEST_IMAGES_PATH = test || TEST_IMAGES_PATH;
+}
+
+function getAllReferenceImages(){
+    return new Promise(function(resolve, reject){
+
+        fs.readdir(REFERENCE_IMAGES_PATH, (err, filenames) => {
+            if (err) {
+                reject('Error Reading folder', err);
+            }
+            resolve(filenames.map((filename)=>({
+                filename,
+                referenceFile: `${REFERENCE_IMAGES_PATH}/${filename}`,
+                testFile: `${TEST_IMAGES_PATH}/${filename}`})
+            ));
+        });
+    });
+}
+
+function createFolder(dir) {
+    if (fs.existsSync(dir)){
+        return fse.emptyDir(dir);
+    }
+    return fs.mkdirSync(dir);
+}
+
+function getTheImageDiff({referenceFile, testFile}) {
+    return new Promise(function (resolve, reject) {
+
+        if (!fs.existsSync(testFile)) {
+            reject(`Test image not found ${testFile}`);
+        }
+
+        resemble.outputSettings(RESEMABLE_SETTINGS);
+
+        resemble(referenceFile)
+            .compareTo(testFile)
+            .onComplete((data) => resolve(data));
+    });
+}
+
+function getAllImageDiff(files) {
+    return files.map((file)=>{
+        let {filename} = file;
+        let test = {
+            filename,
+            status: 'pass'
+        };
+
+        return getTheImageDiff(file)
+            .then((diff)=>{
+                if (diff.isSameDimensions && (diff.misMatchPercentage <= MIS_MATCH_THRESHOLD)) {
+                    return test;
+                }
+
+                test.status = 'fail';
+                saveDiffImage(filename, diff);
+                return test;
+            });
+    });
+}
+
+function saveDiffImage (filename, data) {
+  var fileStream = fs.createWriteStream(`${DIFF_IMAGES_PATH}/${filename}`);
+
+  var storageStream = data.getDiffImage()
+    .pack()
+    .pipe(fileStream);
+
+  return streamToPromise(storageStream);
+}
+
+function RUN() {
+    getConfigFromArgs();
+
+    getAllReferenceImages()
+    .then((files)=>{
+        createFolder(DIFF_IMAGES_PATH);
+        Promise.all(getAllImageDiff(files))
+            .then((d)=>{
+                console.log(d); //eslint-disable-line
+            });
+    }).catch((err)=>err);
+}
+
+RUN();
